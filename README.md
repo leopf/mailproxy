7f1f90d1139ba219daab2e7ea2820e1effd514bc
leopf <l.pfob@3-klicks.de>
2025-11-21 22:54:09 +0000

    some progress on imap

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # mailproxy
 
 (WIP)
+
+## RFCs
+- [IMAP](https://www.rfc-editor.org/rfc/rfc9051.html)
+- [SMTP](https://www.rfc-editor.org/rfc/rfc5321.html)
+- [SMTP AUTH](https://www.rfc-editor.org/rfc/rfc4954.html)
--- mailproxy/bin.py
+++ mailproxy/bin.py
@@ -1,4 +1,4 @@
-import functools, asyncio, dataclasses, re, logging, base64
+import functools, asyncio, dataclasses, re, logging, base64, enum
 
 def match_line(pattern: str, line: str, flags: int = re.I):
   m = re.fullmatch(pattern, line, flags)
@@ -14,15 +14,93 @@ class Config:
   IMAP_port: int = 143
   SMTP_port: int = 587
 
-async def run_imap(config: Config):
-  pass
-
 def smtp_forward_mail(config: Config, sender: str, recipients: tuple[str, ...], data: bytes):
   print("FORWARDING:", data.decode())
+
+def authenticate_sasl(sasl_b64: str):
+  data = base64.b64decode(sasl_b64).split(b"\0")
+  return authenticate(data[1].decode(), data[1].decode())
 
 def authenticate(username: str, password: str) -> bool:
   print("try auth", username, password)
   return True
+
+class IMAPState(enum.Enum):
+  NotAuthenticated = 1
+  Authenticated = 2
+  Selected = 3
+
+async def handle_imap(config: Config, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+  def write_line(line: str):
+    writer.write(line.encode("ascii"))
+    writer.write(b"\r\n")
+
+  async def read_line():
+    return (await reader.readuntil(b"\r\n"))[:-2].decode("utf-8").strip()
+
+  async def parse_line():
+    while True:
+      line = await read_line()
+      pline = match_line(r"(?P<tag>[a-z0-9]+)\s+(?P<rest>.*)", line)
+      if pline is None: continue
+      return pline["tag"], pline["rest"]
+
+  state: IMAPState = IMAPState.NotAuthenticated
+
+  try:
+    write_line(f"220 {config.domain} Ready")
+
+    while not reader.at_eof():
+      tag, line = await parse_line()
+      logging.debug("Client: " + tag + " " + line)
+
+      if match_line(r"CAPABILITY", line):
+        write_line(f"* CAPABILITY IMAP4rev2 AUTH=PLAIN")
+        write_line(f"{tag} OK CAPABILITY completed")
+      elif match_line(r"NOOP", line):
+        if state is IMAPState.Selected:
+          raise NotImplementedError("Need to implement polling updates")
+        else:
+          write_line(f"{tag} OK NOOP completed")
+      elif match_line(r"LOGOUT", line):
+        write_line("* BYE Server logging out")
+        write_line(f"{tag} OK LOGOUT completed")
+      elif match_line(r"AUTHENTICATE PLAIN", line):
+        write_line("+")
+        if (b64_match:=match_line(r"(?P<data>[a-z0-9\+\/]*(=|==)?)", await read_line())) and authenticate_sasl(b64_match["data"]):
+          write_line(f"{tag} OK Success")
+        else:
+          write_line(f"{tag} NO Failed")
+      elif (m:=match_line(r"(?P<mode>(SELECT|EXAMINE)) (?P<mailbox>.*)", line)):
+        raise NotImplementedError()
+        mode = "" # CLOSED, READ-ONLY, READ-WRITE
+        write_line("* FLAGS (\\Answered \\Flagged \\Deleted \\Seen \\Draft)")
+        write_line(f"{tag} OK [{mode}] SELECT completed")
+      elif (m:=match_line(r"CREATE (?P<mailbox>.*)", line)):
+        raise NotImplementedError()
+        # TODO create mailbox, respond no if not allowed or exists
+        write_line(f"{tag} OK CREATE completed")
+      elif (m:=match_line(r"DELETE (?P<mailbox>.*)", line)):
+        raise NotImplementedError()
+        # TODO delete mailbox, respond no if not allowed or not exists
+        write_line(f"{tag} OK DELETE completed")
+      elif (m:=match_line(r"RENAME (?P<mailbox>.*)", line)):
+        raise NotImplementedError()
+        # TODO rename mailbox
+        write_line(f"{tag} OK RENAME completed")
+      elif (m:=match_line(r"SUBSCRIBE .*", line)):
+        write_line(f"{tag} OK SUBSCRIBE completed")
+      elif (m:=match_line(r"UNSUBSCRIBE .*", line)):
+        write_line(f"{tag} NO UNSUBSCRIBE not allowed")
+      else:
+        write_line(f"{tag} NO failed to run command (wrong state or parsing error)")
+
+
+  except Exception as e:
+    logging.error("connection closing because of an error", e)
+  finally:
+    logging.debug("connection closed")
+    writer.close()
 
 async def handle_smtp(config: Config, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
   def write_line(line: str):
@@ -69,8 +147,7 @@ async def handle_smtp(config: Config, reader: asyncio.StreamReader, writer: asyn
 
       # authorization
       elif (m:=match_line(r"AUTH PLAIN (?P<data>[a-z0-9\+\/]*(=|==)?)", line)):
-        data = base64.b64decode(m["data"]).split(b"\0")
-        if authenticate(data[1].decode(), data[1].decode()):
+        if authenticate_sasl(m["data"]):
           reply(235, "2.7.0  Authentication Succeeded")
           authenticated = True
         else:
@@ -120,7 +197,8 @@ async def main():
     log_level=logging.DEBUG,
     database_path="/tmp/mailproxy.sqlite",
     domain="example.com",
-    SMTP_port=1587
+    SMTP_port=1587,
+    IMAP_port=1143,
   )
 
   logging.basicConfig(level=config.log_level)
@@ -128,6 +206,9 @@ async def main():
   async with asyncio.TaskGroup() as tg:
     smtp_server = await asyncio.start_server(functools.partial(handle_smtp, config), config.host, config.SMTP_port)
     tg.create_task(smtp_server.serve_forever(), name="SMTP server")
+
+    imap_server = await asyncio.start_server(functools.partial(handle_imap, config), config.host, config.IMAP_port)
+    tg.create_task(imap_server.serve_forever(), name="IMAP server")
 
 
 if __name__ == "__main__":
